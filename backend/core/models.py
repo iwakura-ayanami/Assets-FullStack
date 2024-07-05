813f2781c3faf4b5040438a0dd8de8ff16eecaf2
asuka <lucas.corcos@utem.cl>
2024-07-05 01:16:18 -0400

    Creacion del model en core

---
+++ backend/core/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Gestor(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Pago(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    gestor = models.ForeignKey(Gestor, on_delete=models.CASCADE)
+    rut = models.CharField(max_length=20)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_pago = models.DateField()
+    abonos = models.IntegerField()
+
+    def __str__(self):
+        return f"Pago de {self.cliente} gestionado por {self.gestor}"
